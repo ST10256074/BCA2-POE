2b27dd6f870304ef34c01910ab2f59d503f741eb
James Hart <st10256074@vcconnect.edu.za>
2024-04-10 13:10:17 +0200

    added requirements as comments

--- Program.cs
+++ Program.cs
@@ -1,15 +1,36 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BCA2_POE
 {
     internal class Program
     {
         static void Main(string[] args)
+
         {
+//1.The user shall be able to enter the details for a single recipe:
+//a.The number of ingredients.
+//b.For each ingredient: the name, quantity, and unit of measurement.For example, one
+//tablespoon of sugar.
+//c.The number of steps.
+//d.For each step: a description of what the user should do.
+//2.The software shall display the full recipe, including the ingredients and steps, in a neat
+//format to the user.
+//3.The user shall be able to request that the recipe is scaled by a factor of 0.5(half), 2
+//(double), or 3(triple).All the ingredient quantities shall be changed accordingly when the
+//recipe is displayed.For example, one tablespoon of sugar will become two tablespoons of
+//sugar if the factor is 2.
+//4.The user can request that the quantities be reset to the original values.
+//5.The user shall be able to clear all the data to enter a new recipe.
+//6.The software shall not persist the user data between runs.The data shall only be stored in
+//memory while the software is running.
+
         }
     }
 }
